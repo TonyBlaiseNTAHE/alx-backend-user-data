aa90fe66b4f074ac8b3a6e92f815b9dbf4d61461
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-12-21 12:52:57 +0200

    updates

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,27 +1,68 @@
 #!/usr/bin/env python3
-"""doc doc doc """
+"""
+Module for authentication
+"""
+
+
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
-    """doc doc doc"""
+    """_summary_
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """doc doc doc"""
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
-            return True
-        if path[-1] != "/":
-            path += "/"
+        """_summary_
 
-        return path not in excluded_paths
+        Args:
+                path (str): _description_
+                excluded_paths (List[str]): _description_
+
+        Returns:
+                bool: _description_
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """doc doc doc"""
+        """_summary_
+
+        Args:
+                request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+                str: _description_
+        """
         if request is None:
             return None
-        return request.headers.get("Authorization", None)
+        # get header from the request
+        header = request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar("User"):
-        """doc doc doc"""
+        if header is None:
+            return None
+
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """_summary_
+        """
+
         return None
